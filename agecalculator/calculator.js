03ce8cb9333b11c33e49e968fe648cbb1f9f56de
unknown <19chrisg97@gmail.com>
2020-02-22 13:00:54 -0500

    added error message when someone gives an invalid birthday

--- agecalculator/calculator.js
+++ agecalculator/calculator.js
@@ -1,13 +1,17 @@
 function calculateAge(birthday) {
     var currentDate = new Date();
     var birthDate = new Date(birthday);
-    return ((currentDate - birthDate) / 1000 / 60 / 60 / 24 / 365.2422).toFixed(8);
+    if (birthDate == "Invalid Date") {
+        return "Insert a Valid Birthday";
+    } else {
+        return ((currentDate - birthDate) / 1000 / 60 / 60 / 24 / 365.2422).toFixed(8);
+    }
 }
 
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
+    var expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
@@ -15,7 +19,7 @@ function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
@@ -38,5 +42,6 @@ if (getCookie("AgeCalculatorBirthday") == "") {
     setCookie("AgeCalculatorBirthday", birthday, 365)
 }
 savedBirthday = getCookie("AgeCalculatorBirthday");
-window.setInterval(function() {
-    document.getElementById("age").innerHTML = calculateAge(savedBirthday)}, 100);
+window.setInterval(function () {
+    document.getElementById("age").innerHTML = calculateAge(savedBirthday)
+}, 100);
