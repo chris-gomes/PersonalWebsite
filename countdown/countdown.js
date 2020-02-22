1b221d4a34f312ee17a8787cb987a802747e292d
unknown <19chrisg97@gmail.com>
2020-02-22 12:16:03 -0500

    fixed calc bug and get element bug in countdown

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -1,29 +1,38 @@
-function calcTimeValues(date) {
+function getTimeValues(date) {
     var currentDate = new Date();
     var eventDate = new Date(date);
-    if (currentDate >= eventDate) {
-        return [0, 0, 0, 0];
+    var days = 0;
+    var hours = 0;
+    var minutes = 0;
+    var secondsLeft = 0;
+
+    if (currentDate < eventDate) {
+        secondsLeft = Math.floor((eventDate - currentDate) / 1000);
+        days = Math.floor(secondsLeft / 60 / 60 / 24);
+        secondsLeft = secondsLeft - (days * 24 * 60 * 60);
+        hours = Math.floor(secondsLeft / 60 / 60);
+        secondsLeft = secondsLeft - (hours * 60 * 60);
+        minutes = Math.floor(secondsLeft / 60);
+        secondsLeft = secondsLeft - (minutes * 60);
     }
-    var secondsLeft = Math.floor((eventDate - birthDate) / 1000);
-    var days = Math.floor(secondsLeft / 60 / 60 / 24);
-    secondsLeft = secondsLeft - (days * 24 * 60 * 60);
-    var hours = Math.floor(secondsLeft / 60 / 60);
-    secondsLeft = secondsLeft - (hours * 60 * 60);
-    var minutes = Math.floor(secondsLeft / 60);
-    secondsLeft = secondsLeft - (minutes * 60);
-    return [days, hours, minutes, secondsLeft];
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = secondsLeft;
 }
 
-// function setCookie(cname, cvalue) {
-//     var d = new Date();
-//     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-//     var expires = "expires=" + d.toUTCString();
-//     document.cookie = cname + "=" + cvalue;
-// }
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
@@ -39,26 +48,18 @@ function getCookie(cname) {
 function resetNameAndDate() {
     var name = prompt("Event Name:");
     var date = prompt("Event Date (Month DD YYYY):");
-    document.cookie = "CountdownEventName" + "=" + name;
-    document.cookie = "CountdownEventDate" + "=" + date;
+    setCookie("CountdownEventName", name, 365);
+    setCookie("CountdownEventDate", date, 365);
     location.reload(true);
-}
-
-function updateHTML(name, timeValues) {
-    document.getElementById("name").innerHTML = name;
-    document.getElementById("days").innerHTML = timeValues[0];
-    document.getElementById("hours").innerHTML = timeValues[1];
-    document.getElementById("minutes").innerHTML = timeValues[2];
-    document.getElementById("seconds").innerHTML = timeValues[3];
 }
 
 if (getCookie("CountdownEventName") == "" || getCookie("CountdownEventDate") == "") {
     var name = prompt("Event Name:");
     var date = prompt("Event Date (Month DD YYYY):");
-    document.cookie = "CountdownEventName" + "=" + name;
-    document.cookie = "CountdownEventDate" + "=" + date;
+    setCookie("CountdownEventName", name, 365);
+    setCookie("CountdownEventDate", date, 365);
 }
-savedName = getCookie("CountdownEventName");
-savedDate = getCookie("CountdownEventDate");
-timeValues = calcTimeValues(savedDate)
-window.setInterval(updateHTML, 100, savedName, timeValues);
+var savedName = getCookie("CountdownEventName");
+var savedDate = getCookie("CountdownEventDate");
+document.getElementById("name").innerHTML = savedName;
+window.setInterval(getTimeValues, 100, savedDate);
